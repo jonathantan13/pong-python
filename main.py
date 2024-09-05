baadad6d8fa11abc3a990ec4e9c8ddcf83d8b89b
Jonathan <jtan13122004@gmail.com>
2024-09-05 23:01:57 +0800

    Removed some obsolete variables as well as add some documentation in the game loop, this is actually the final final version.

--- main.py
+++ main.py
@@ -10,15 +10,12 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-scoreboard = Scoreboard()
-
 game_on = True
-player_1_score = 0
-player_2_score = 0
 
 player_1 = Paddle(350)
 player_2 = Paddle(-350)
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player_1.go_up, "Up")
@@ -31,13 +28,16 @@ while game_on:
     screen.update()
     ball.move()
 
+    #Checks if ball collides with floor or ceiling, if true then ball bounces up/down
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce()
 
+    #Checks if ball collides with paddle by checking the distance between the ball and the paddle and the ball's x coordinate
     if ball.distance(player_1) < 50 and ball.xcor() > 320 or ball.distance(player_2) < 50 and ball.xcor() < -320:
         ball.hit_paddle()
         ball.increase_speed()
 
+    #Check if ball goes out of bounds, if true then reset ball and increment player's score.
     if ball.xcor() > 400:
         scoreboard.p2_point()
         ball.reset_pos()
